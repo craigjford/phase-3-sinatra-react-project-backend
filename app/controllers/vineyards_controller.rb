a8d4de3b907c1d76cf8f264cdc1aaec0a636a7c0
Craig Ford <craig_ford@icloud.com>
2022-09-15 18:15:36 -0400

    deleted singleton vineyard GET from controller

--- app/controllers/vineyards_controller.rb
+++ app/controllers/vineyards_controller.rb
@@ -2,24 +2,14 @@ class VineyardsController < ApplicationController
  
     get '/vineyards' do
         vineyards = Vineyard.all
-        vineyards.to_json
-    end    
-
-    get '/vineyards/:id' do
-        vineyards = Vineyard.find(params[:id])
-        if vineyards
-            vineyards.to_json(include: :wines)
-        else
-            '404 - Vineyard not found'
-        end    
-    end    
+        vineyards.to_json(include: :wines)
+    end      
 
     delete '/vineyards/:id' do
         vineyard = Vineyard.find(params[:id])
         vineyard.destroy
         vineyard.to_json   
     end  
-
 
     post '/vineyards' do
         vineyard = Vineyard.create(name: params[:name], address: params[:address],
