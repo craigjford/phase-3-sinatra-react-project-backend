7bfc0634bedc7657d29c95f8d406a1c217df6393
Craig Ford <craig_ford@icloud.com>
2022-09-20 12:57:50 -0400

    Add code to Wine controller for PATCH

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -5,15 +5,6 @@ class WinesController < ApplicationController
         wines.to_json
     end 
 
-    # post '/wines' do
-    # # post 'vineyards/:vineyard_id/wines' do 
-    #     # vinyard = Vineyard.find_by(id: params[:vineyard_id])
-    #     # wine = Vinyard.wines.create(params)
-    #     wine = Wine.create(params)
-    #     wine.to_json
-    # end
-
-
     post '/wines' do
         wine = Wine.create(name: params[:name], price: params[:price],
                 vineyard_id: params[:vineyard_id], year: params[:year])
@@ -26,15 +17,10 @@ class WinesController < ApplicationController
         wine.to_json   
     end
 
-    # delete '/wines/:id' do
-    #     wine = Wine.find_by(params[:id])
-    #     wine.destroy
-    # end
-
-    # patch '/wines/:id' do
-    #     wine = Wine.find_by(params[:id])
-    #     wine.update(name: params[:name], price: params[:price])  
-    #     wine.to_json
-    # end
+    patch '/wines/:id' do
+        wine = Wine.find_by(params[:id])
+        wine.update(name: params[:name], price: params[:price], year: params[:year])  
+        wine.to_json
+    end
 
 end
